b924995eb8555623f30b5949e03af91d84665b28
Jerry12138jerry <2473047707@qq.com>
2018-11-05 13:27:40 +0800

    Initial commit

---
+++ test/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/2 18:15
+# @Author  : Jerry
+# @Site    :
+# @File    : test.py
+# @Software: PyCharm
+varchar = '''hello
+world
+'''
+list = ["你好","世界"]
+
+print(list)
+
+list.append("我也好世界")
+
+print(list)
+
+del list[1]
+
+print(list)
+
+fibs=(1,2,3)
+
+print(fibs[0])
