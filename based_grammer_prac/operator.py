324a9fa871f0eb56bff49a63c1f8ec0608d1aa9f
Jerry12138jerry <2473047707@qq.com>
2018-11-07 17:53:24 +0800

    python操作符小例子

---
+++ based_grammer_prac/operator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/7 17:09
+# @Author  : Jerry
+# @Site    : 
+# @File    : operator.py
+# @Software: PyCharm
+
+a = "hello"
+b = "world"
+
+# "+"字符串拼接
+print(a+b)
+# "*"重复输出字符串
+print(a*2)
+
+print(a[0])
+
+print(a[1:4])
